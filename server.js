b2d9b0b494477bffc90c2cf491cad04f4c65c0b2
Saif ur rehman <m.awan.saifurrehman@gmail.com>
2023-01-02 13:56:05 +0500

    cors origin

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  })
+);
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+saifurre;
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -22,5 +28,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log("listening on *:3000");
+  console.log(`listening on *: ${port}`);
 });
